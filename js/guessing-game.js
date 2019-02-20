8dbfd8fd8a4ef92d7f9cbb4847024a798fb4e4a4
Daniela Mead <danielamead3@gmail.com>
2019-02-20 07:13:11 -0500

    guessing game

--- js/guessing-game.js
+++ js/guessing-game.js
@@ -1,9 +1,76 @@
-/* 
+const generateWinningNumber = () => {
+  return Math.ceil(Math.random() * 100);
+}
 
-Write your guess-game code here! Don't forget to look at the test specs as a guide. You can run the specs
-by running "testem".
+class Game {
+  constructor() {
+    this.playersGuess = null;
+    this.winningNumber = generateWinningNumber();
+    this.pastGuesses = [];
+  }
 
-In this file, you will also include the event listeners that are needed to interact with your HTML file when
-a user clicks a button or adds a guess to the input field.
+  difference() {
+    return Math.abs(this.playersGuess - this.winningNumber);
+  }
 
-*/
+  isLower() {
+    return this.playersGuess < this.winningNumber;
+  }
+
+  playersGuessSubmission(guess) {
+    if (typeof guess === 'number' && guess >= 1 && guess <= 100) {
+      this.playersGuess = guess;
+    } else {
+      document.querySelector('#feedback').innerHTML = 'That is an invalid guess.';
+    }
+    return this.checkGuess();
+  }
+
+  checkGuess() {
+    let feedback = '';
+
+    if (this.playersGuess === this.winningNumber) {
+      feedback = 'You Guessed It! Have Some Pie!';
+    } else if (this.pastGuesses.includes(this.playersGuess)) {
+      feedback = 'You have already guessed that number.';
+    } else {
+      this.pastGuesses.push(this.playersGuess);
+      if (this.pastGuesses.length === 5) {
+        feedback = 'You Shall Have No Pie!';
+      } else {
+        let diff = this.difference();
+        if (diff < 10) feedback = "You're burning up!";
+        else if (diff < 25) feedback = "You're lukewarm.";
+        else if (diff < 50) feedback = "You're a bit chilly.";
+        else feedback = "You're ice cold!";
+      }
+    }
+    document.querySelector('#feedback').innerHTML = feedback;
+    document.querySelector(`#guess${this.pastGuesses.length}`).innerHTML = this.playersGuess;
+  }
+}
+
+const newGame = () => {
+  return new Game();
+}
+
+const playGame = () => {
+  const game = newGame();
+  const button = document.querySelector('button');
+
+  button.addEventListener('click', function() {
+    const playersGuess = +document.querySelector('input').value;
+    document.querySelector('input').value = '';
+
+    game.playersGuessSubmission(playersGuess);
+  });
+}
+
+{
+  const playAgain = document.getElementById('reset');
+  playAgain.addEventListener('click', function() {
+    location.reload();
+  });
+}
+
+playGame();
